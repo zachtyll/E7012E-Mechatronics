b7ecd671d660914705b933a8d0148262cc9a0075
Zacharias Tyllström <zache21@hotmail.se>
2019-04-17 16:30:16 +0200

    Integrated the HALL file into the rest of the program.

--- Arduino/C-code/line_rider/Mek2/HALL.ino
+++ Arduino/C-code/line_rider/Mek2/HALL.ino
@@ -1,31 +1,14 @@
-float circ=0.065*3.14; //circumference of wheeel
-float vel=0;
-float lastTime=0;
-float timevel;
-float lastVel;
-void setup() {
-  Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(3), sensor, RISING);  
-  
-
-}
-
-void loop() {
-  Serial.print(vel);
-  digitalWrite(3, 1);
-  delay(1000);
-  digitalWrite(3, 0);
-  lastVelocity;
-}
-
-void sensor(){
+void Sensor(){
+    Serial.println("Sensor TRIGGERED!");
     if (millis()!=lastTime){
       timevel=(millis()-lastTime)*1000;
       vel=circ*0.5/timevel;
       lastTime=millis();
       lastVel=vel;}    
-  }
-void lastVelocity(){
+}
+void LastVelocity(){
+  Serial.println("oTHER TRIGGERED!");
   if (lastVel!=vel) {
   Serial.println(vel);
-  }}
+  }
+}
--- Arduino/C-code/line_rider/Mek2/Mek2.ino
+++ Arduino/C-code/line_rider/Mek2/Mek2.ino
@@ -2,28 +2,41 @@
 #include <Servo.h>                      //include the servo library
 
 Servo SteeringServo;                    // Servo object Servo
-Servo MotorServo;                       // Servo object Motorcontroll
+Servo MotorServo;                       // Servo object Motorcontrol
 
+float circ=0.065*3.14;                  //circumference of wheeel
+float vel=0;
+float lastTime=0;
+float timevel;
+float lastVel;
 float angle = 0.75;                      // Steering angle -1 is left 0 is straigth and 1 is right. X100 for % 
 float mps = 0.75;                        // Engine speed where 0 is standing still and 1 is full throttle
 
-void setup() {
-  Serial.begin(9600);                        // Sets the data rate in bits per second (baud) for serial data transmission.
+void setup() { 
+  
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(3), Sensor, RISING);
+  interrupts();
+                                             // Sets the data rate in bits per second (baud) for serial data transmission.
   MotorSetup();                              // Engine function to tab for setup
   ServoSetup();                              // Servo function to tab for setup
   Serial.println("Setup completed!");
   delay(1000);
 }
 void loop() {
-  //SetSpeed(mps);                               //Makes function for running the motortab        
-  SetSteering(angle);                            //Makes function for running the servotab
-  angle = 0.5;
-  SetSteering(angle);
-  
-  delay(3000);                              //demonstration time of 3 sek until reset below                      
-  MotorServo.writeMicroseconds(1200);       //set speed to 0
-  SteeringServo.writeMicroseconds(1550);    //set steering straight 
-  delay(2000);                              //Keep it zero for 2 sek until showing desierd values again.
+  digitalWrite(3, HIGH);
+  delay(1000);
+  digitalWrite(3, LOW);
+
+//  SetSpeed(mps);                               //Makes function for running the motortab        
+//  SetSteering(angle);                            //Makes function for running the servotab
+//  angle = 0.5;
+//  SetSteering(angle);
+//  
+//  delay(3000);                              //demonstration time of 3 sek until reset below                      
+//  MotorServo.writeMicroseconds(1200);       //set speed to 0
+//  SteeringServo.writeMicroseconds(1550);    //set steering straight 
+//  delay(2000);                              //Keep it zero for 2 sek until showing desierd values again.
 }
 
   
