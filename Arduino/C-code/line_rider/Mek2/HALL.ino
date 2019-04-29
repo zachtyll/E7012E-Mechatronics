c5cc540344211df811e773665a77b78a023ece2e
A1203-admin <zache21@hotmail.se>
2019-04-29 15:24:50 +0200

    Added some code to the HALL tab. It should in theory remove any transients and act as a low-pass filter.

--- Arduino/C-code/line_rider/Mek2/HALL.ino
+++ Arduino/C-code/line_rider/Mek2/HALL.ino
@@ -5,7 +5,7 @@ float previousVelocity = 0;
 const float circumference = 0.065* 3.14;    //circumference of wheel
 
 void Sensor(){
-  if (millis() > previousTime + 15){                  // Ensure no double detection
+  if (millis() > previousTime + 15){                  // Ensure no double detection by 'low-pass filtering'
     hallPeriod = (millis() - previousTime) / 1000;    // Calculates the time since last trigger of the sensor.
     hallVelocity = circumference * 0.5 / hallPeriod;  // Calculates the speed the car has reached.
     previousTime = millis();                          // Set previous time for next loop
